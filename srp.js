125faca2a3a393eef6187167d26d9672068bb143
Stanislav Stashchuk <stashchuk_stanislav@ukr.net>
2021-11-03 00:43:14 +0200

    first js commit Some JS #2

---
+++ srp.js
@@ -0,0 +1,11 @@
+const userScore = 0
+const compScore = 0
+const userScoreSpan = document.getElementById("user-score")
+const compScoreSpan = document.getElementById("comp-score")
+const scoreBoard = document.querySelector(".score-board")
+const result = document.querySelector(".result")
+
+const rock = document.getElementById("brick")
+const paper = document.getElementById("parchment")
+const scissors = document.getElementById("scissors")
+const button = document.getElementById("button")
